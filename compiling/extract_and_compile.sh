20872d1c04e16d009fef15ce89c0561cbb70f22e
El Anderson <and10393@umn.edu>
2025-08-26 11:31:28 -0500

    updated extract_and_compile to pass shellcheck
    removed unused variable, added exit option when entering a directory, added double quotes to output line

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/bash
 
-int=$1
-
 tar zxf ./NthPrime.tgz # uncompress and extract directory
-cd NthPrime # enter directory
+cd NthPrime || exit # enter directory or quit
 
 gcc -o NthPrime nth_prime.c main.c # compile two c files into one named NthPrime
 
-./NthPrime $1 # run compiled code
+"./NthPrime $1" # run compiled code with command line input
 
 # rm -rf NthPrime # housekeeping
