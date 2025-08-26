f1fc1d1802f5a5887ae92ab2672d1752d2f8ddd0
El Anderson <and10393@umn.edu>
2025-08-26 11:21:07 -0500

    Implements extract_and_compile
    all bat tests now pass

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/bash
+
+int=$1
+
+tar zxf ./NthPrime.tgz # uncompress and extract directory
+cd NthPrime # enter directory
+
+gcc -o NthPrime nth_prime.c main.c # compile two c files into one named NthPrime
+
+./NthPrime $1 # run compiled code
+
+# rm -rf NthPrime # housekeeping
