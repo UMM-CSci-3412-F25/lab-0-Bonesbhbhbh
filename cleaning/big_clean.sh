2c4725ebaebec3a45500549734189ac426e8b57b
El Anderson <and10393@umn.edu>
2025-08-28 11:40:42 -0500

    progress on clean_big
    now passes 4 tests

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -2,10 +2,17 @@
 
 directory=$1
 
-SCRATCH=`mktemp --directory`
+SCRATCH=$(mktemp --directory WOMP-XXXXXX)
+here=$(pwd)
 
-tar zxf $directory --directory $SCRATCH
+tar zxf "$directory" --directory "$SCRATCH" # open directory to temp dir
 
-ls
+to_delete=$(grep -lr "DELETE ME" "$SCRATCH")
+# echo `basename $to_delete -a`
+grep -lrZ "DELETE ME" "$SCRATCH" | xargs -0 rm -f
+tree "$SCRATCH"
 
+cd "$SCRATCH"
+tar -czf cleaned_"$directory" "$directory"
+mv cleaned_"$directory" "$here"
 rm -rf $SCRATCH
